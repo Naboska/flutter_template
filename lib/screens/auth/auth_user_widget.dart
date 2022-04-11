5b67e6ab56470f5b0a33c583785f19ef586d1318
Nabos <nabos.dezigner@gmail.com>
2022-04-11 11:32:37 +0300

    feat: fix sorted

--- lib/screens/auth/auth_user_widget.dart
+++ lib/screens/auth/auth_user_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:flutter_template/blocs/user/user_bloc.dart';
 
 class AuthUserWidget extends StatelessWidget {
